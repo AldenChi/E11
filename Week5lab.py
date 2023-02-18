7130b5c449824a5cf84782c43cafffadbf0e85fd
AldenChi <124406923+AldenChi@users.noreply.github.com>
2023-02-17 18:22:53 -0800

    Update Week5lab.py

--- Week5lab.py
+++ Week5lab.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
-
 import sys
 import random
 import time
@@ -13,31 +11,18 @@ import board
 import busio
 from digitalio import DigitalInOut, Direction, Pull
 from adafruit_pm25.i2c import PM25_I2C
-    
-    
 
 i2c=board.I2C()
 bme680=adafruit_bme680.Adafruit_BME680_I2C(i2c)
-bme680.sea_level_pressure=1013.25
-     
-    
+bme680.sea_level_pressure=1013.25 
 
 reset=None
-
-     
-
 
 import serial
 uart=serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.95)
 
-     
-
-
 from adafruit_pm25.uart import PM25_UART
 pm25=PM25_UART(uart,reset)
-
-     
-
 
 import csv
 meta_data=["Time", "PM10 std", "PM25 std", "PM100 std", "Temperature", "Gas", "Pressure", "Altitude", "Relative Humididty"]
@@ -48,7 +33,6 @@ print(sys.argv)
 start=int(time.time())
 run=int(sys.argv[1])
 itime=start
-     
 
 while itime<(start+run):
     time.sleep(1)
