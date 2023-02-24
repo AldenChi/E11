4e28d781da02125a1fb6f1985903cc1546d1d211
AldenChi <124406923+AldenChi@users.noreply.github.com>
2023-02-24 14:53:53 -0800

    Update Week5lab.py

--- Week5lab.py
+++ Week5lab.py
@@ -32,9 +32,9 @@ writer.writerow(meta_data)
 print(sys.argv)
 start=int(time.time())
 run=int(sys.argv[1])
-itime=start
+itime=start+run+300
 
-while itime<(start+run):
+while itime>(start+run):
     time.sleep(1)
     try:
         itime = time.time()
